b642d88057730c227e8a587440bb260eab81269d
Daniela <daniela@gmail.com>
2025-01-14 15:27:38 +0100

    Borro cambios realizados por Lucia

--- MiClase1.java
+++ MiClase1.java
@@ -7,8 +7,7 @@ public class MiClase1
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
             
         }
-        {System.out.println("Hola Mundo");}
-        {System.out.println("¿Cómo estás?");}
+
         {System.out.println("Cambio de Marcelo");}
         
     }
