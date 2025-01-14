25ba61a42e5945b8d45218afb644703316df5b0a
Lucia Rivadulla <lucia@gmail.com>
2025-01-14 15:06:37 +0100

    Agrego Cómo estás?

--- MiClase1.java
+++ MiClase1.java
@@ -5,6 +5,10 @@ public class MiClase1
         for(int i = 0; i <= 10; i++)
         {
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
+            
         }
+        {System.out.println("Hola Mundo");}
+        {System.out.println("¿Cómo estás?");}
+        
     }
 }
